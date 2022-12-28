b4c432cda2c52741427c268a51c450640a807867
vamsi-012 <vamshikatikala1@gmail.com>
2022-12-28 13:52:33 +0000

    routing practice app

---
+++ src/components/About/index.js
@@ -0,0 +1,16 @@
+// Write your JS code here
+import './index.css'
+
+const About = () => (
+  <div className="container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/about-blog-img.png"
+      alt="about"
+      className="image"
+    />
+    <h1 className="heading">About</h1>
+    <p className="para">I love to create! Iam a front-end web developer</p>
+  </div>
+)
+
+export default About
---
+++ src/components/Header/index.js
@@ -0,0 +1,36 @@
+// Write your JS code here
+import {Link} from 'react-router-dom'
+
+import './index.css'
+
+const Header = () => (
+  <nav className="main-container">
+    <div className="container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/wave-logo-img.png"
+        className="logo-image"
+        alt="wave"
+      />
+      <h1 className="title">Wave</h1>
+    </div>
+    <ul className="components">
+      <li className="link">
+        <Link className="component-link" to="/">
+          Home
+        </Link>
+      </li>
+      <li className="link">
+        <Link className="component-link" to="/about">
+          About
+        </Link>
+      </li>
+      <li className="link">
+        <Link className="component-link" to="/contact">
+          Contact
+        </Link>
+      </li>
+    </ul>
+  </nav>
+)
+
+export default Header
